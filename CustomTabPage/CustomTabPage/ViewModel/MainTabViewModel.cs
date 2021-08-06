45ff53f07872badfcbf173d1c7696dd69d3adfc5
dieguinsharp <diegoomaispika@outlook.com>
2021-08-06 20:52:20 -0300

    ajustes nos testes

--- CustomTabPage/CustomTabPage/ViewModel/MainTabViewModel.cs
+++ CustomTabPage/CustomTabPage/ViewModel/MainTabViewModel.cs
@@ -22,26 +22,26 @@ namespace CustomTabPage.ViewModel {
 
             StackLayout page = null;
 
-            var pageId = Convert.ToInt32(id);
+            var pageId = (Page)Convert.ToInt32(id);
 
             switch(pageId) {
 
-                case 1:
+                case Page.Page1:
 
                     page = new Page1();
 
                     break;
-                case 2:
+                case Page.Page2:
 
                     page = new Page2();
 
                     break;
-                case 3:
+                case Page.Page3:
 
                     page = new Page3();
 
                     break;
-                case 4:
+                case Page.Page4:
 
                     page = new Page4();
 
@@ -55,5 +55,12 @@ namespace CustomTabPage.ViewModel {
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    enum Page {
+        Page1 = 1,
+        Page2 = 2,
+        Page3 = 3,
+        Page4 = 4
     }
 }
